383134e8906709fef00accbca874ffb388267eca
cuong nguyen <cuongnguyend863@gmail.com>
2020-09-09 20:03:32 +0700

    cm

--- js/layout.js
+++ js/layout.js
@@ -1,27 +1,41 @@
-var countDownDate = new Date("april 8, 2025 15:37:25").getTime();
+window.addEventListener('load', function() {
+    $('#item-example').on('slide.bs.carousel', function() {
+        var countDownDate = new Date("april 8, 2025 15:37:25").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+        var x = setInterval(function() {
 
-    // Get today's date and time
-    var now = new Date().getTime();
+            var now = new Date().getTime();
 
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+            var distance = countDownDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Output the result in an element with id="demo"
-    document.getElementById("time_countdown").innerHTML = days + ":  " + hours + ":  " +
-        minutes + ":  " + seconds;
+            document.getElementsByClassName("time_countdown").innerHTML = days + ":  " + hours + ":  " +
+                minutes + ":  " + seconds;
 
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("time_countdown").innerHTML = "EXPIRED";
-    }
-}, 1000);
+            if (distance < 0) {
+                clearInterval(x);
+                document.getElementsByClassName("time_countdown").innerHTML = "EXPIRED";
+            }
+        }, 1000);
+    })
+})
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 20) {
+            $('#toTopBtn').fadeIn();
+        } else {
+            $('#toTopBtn').fadeOut();
+        }
+    });
+
+    $('#toTopBtn').click(function() {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+        return false;
+    });
+});
